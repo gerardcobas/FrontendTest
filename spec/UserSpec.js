445e892ff52dff5fb42c5919d810c216c1373bda
Leke Abolade <leke_abolade92@hotmail.co.uk>
2017-02-22 16:03:05 +0000

    adding method to create repository

--- spec/UserSpec.js
+++ spec/UserSpec.js
@@ -4,14 +4,16 @@
     var fullName;
     var username;
     var avatar;
-    var repositories;
+    var repositoryName;
+    var repositoryStars;
+    var repositoryForks;
 
     beforeEach(function() {
       fullName = "fullName";
       username = "username";
       avatar = "avatar";
       repositories = "repositories";
-      user = new User(fullName, username, avatar, repositories);
+      user = new User(fullName, username, avatar);
     });
 
     it("has a full name", function() {
@@ -30,6 +32,10 @@
       expect(user.repositories).toBeDefined()
     });
 
+    it("has an array of repositories", function() {
+      expect(user.repositories.constructor.name).toEqual("Array");
+    });
+
     describe("instantiation", function() {
       it("saves the full name", function() {
         expect(user.fullName).toEqual(fullName);
@@ -42,9 +48,25 @@
       it("saves the avatar", function() {
         expect(user.avatar).toEqual(avatar);
       });
+    });
 
-      it("saves the repositories", function() {
-        expect(user.repositories).toEqual(repositories);
+    describe("can make a new repository", function() {
+      beforeEach(function() {
+        repositoryName = "name";
+        repositoryStars = 1;
+        repositoryForks = 1;
+        user.createRepository(repositoryName, repositoryStars, repositoryForks)
+      })
+
+      it("pushes to the repositories array", function() {
+        expect(user.repositories.length).toEqual(1);
+      });
+
+      it("stores the correct details from the method", function() {
+        var createdRepository = user.repositories[0];
+        expect(createdRepository.name).toEqual(repositoryName);
+        expect(createdRepository.forks).toEqual(repositoryForks);
+        expect(createdRepository.stars).toEqual(repositoryStars);
       });
     });
   });
