47c42c60cb9388c4f759488b5c135670f9b49b35
CodeMyCoffee <codemycoffee@gmail.com>
2017-04-04 08:22:52 +0200

    Added js function structure

---
+++ main.js
@@ -0,0 +1,36 @@
+//set initial global variables
+var username;
+var result;
+
+//AJAX base request
+function request(url, callback) {
+    
+}
+
+//request user data and repos
+function requestUser(username) {
+
+}
+
+//fill result div with data
+function fillResultDetails(data) {
+    
+}
+
+//displays not found or empty error
+function showError(err) {
+    
+}
+
+
+//search button function
+function submitUser() {
+
+    username = document.getElementById('user').value
+    result = document.getElementById('result');
+
+}
+
+
+
+
