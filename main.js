b25f8347bb74e05f8a566d0722783080a4c90828
CodeMyCoffee <codemycoffee@gmail.com>
2017-04-04 08:55:18 +0200

    Get repos implemented

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function request(url, callback) {
     xmlHttp.send(null);
 }
 
-//request user data and repos
+//request user data
 function requestUser(username) {
 
     const baseUrl = "https://api.github.com/users/" + username;
@@ -25,11 +25,27 @@ function requestUser(username) {
     request(baseUrl, (data) => {
         if (data != "404") {
             fillResultDetails(JSON.parse(data));
+            //if user exists then request repos
+            requestRepos(username)
         } else {
-            showError("404");
+            showError("nouser");
         }
     })
 }
+
+//request repos from user
+function requestRepos(username){
+    const baseUrl = "https://api.github.com/users/" +username+"/repos";
+
+    request(baseUrl, (data) => {
+        if (data != "404") {
+            fillReposDetails(JSON.parse(data));
+        } else {
+            showError("norepos");
+        }
+    })    
+}
+
 
 //fill result div with data
 function fillResultDetails(data) {
@@ -40,20 +56,49 @@ function fillResultDetails(data) {
 
     var output = '<img src="' + avatar + '" alt="avatar">' +
         '<p>@' + login + '</p>' +
-        '<h3>' + fullname + '</h3>' +
+        '<h2>' + fullname + '</h2>' +
         '<p>' + bio + '</p>';
 
     result.innerHTML = output;
     result.className = "success"
 }
 
+//fill repos div with data
+function fillReposDetails(data){
+
+    const repos = document.getElementById('repos');
+    repos.style.display = "inherit";
+
+    var output = '<h3>Repos</h3><hr>'
+
+    data.forEach((repo)=>{
+        const name = repo.name;
+        const stars = repo.stargazers_count;
+        const forks = repo.forks_count;
+
+        output+= '<div class="repo"><h3>'+name+'</h3>'+
+        '<span class="rDetails">'+
+        '<p>'+stars+'</p>'+
+        '<p>'+forks+'</p>'+
+        '</span>'+
+        '<hr></div>'
+    });
+
+    repos.innerHTML = output;
+    repos.className = "reposuccess";
+}
+
 //displays not found or empty error
 function showError(err) {
-    result.className = "error";
     if (err == "empty") {
+        result.className = "error";
         result.innerHTML = "Empty field, please type a username";
-    } else {
+    } else if(err == "nouser"){
+        result.className = "error";
         result.innerHTML = "User not found";
+    } else{
+        repos.className = "error";
+        repos.innerHTML = "No repos found";
     }
 }
 
