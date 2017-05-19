eae084105e7e85c5245fafcbbae05bb07d5d7571
Josep Casado <josepcasado94@gmail.com>
2017-05-19 21:24:36 +0200

    Main functionality ready.

--- js/trial.js
+++ js/trial.js
@@ -5,7 +5,10 @@ GithubApi = {
     settings: {
         client: null,
         submitButton: document.getElementById("submit"), 
-        inputUsername: document.getElementById("githubUsername")
+        inputUsername: document.getElementById("githubUsername"),
+        alertBox: document.getElementById("alert"),
+        userInfoRepos: document.getElementById("user-info-repos"),
+        userNotFoundMessage: "Not Found"
     },
 
     init: function() {
@@ -13,19 +16,14 @@ GithubApi = {
         s = this.settings;
         s.client = new this.httpClient()
         this.bindUIActions();
-        // json = this.fetchUserInfo(s.client, "caspyin")
-        // this.fetchUserInfo(function(json) {
-        //     console.log(json)
-        // })
 
     },
 
     bindUIActions: function() {
 
         s.submitButton.addEventListener("click", function(){
-            GithubApi.searchForUser(function(json) {
-                console.log(json)
-            });
+            s.userInfoRepos.style.display = 'none'
+            GithubApi.analyzeQuery()
         });
 
         s.inputUsername.addEventListener("keyup", function(event){
@@ -38,13 +36,15 @@ GithubApi = {
     httpClient: function() {
         this.get = function(url, callback) {
             var req = new XMLHttpRequest();
-            req.onreadystatechange = function() { 
-                if (req.readyState == 4 && req.status == 200)
-                    callback(req.responseText);
+            req.onreadystatechange = function() {
+                if (req.readyState == 4) {
+                    if (req.status == 200 || req.status == 404)
+                        callback(req.responseText)
+                }
             }
 
-            req.open( "GET", url, true );            
-            req.send( null );
+            req.open("GET", url, true);            
+            req.send();
         }
     },
 
@@ -56,6 +56,66 @@ GithubApi = {
             callback(JSON.parse(response));
 
         });
+    },
+
+    searchForRepos: function(callback) {
+
+        s.client.get("https://api.github.com/users/" + s.inputUsername.value + "/repos?per_page=100", function(response) {
+
+            //jsonize the response
+            callback(JSON.parse(response));
+
+        })
+
+    },
+
+    analyzeQuery: function() {
+
+        this.searchForUser(function(jsonUser) {
+
+                if (jsonUser["message"] == s.userNotFoundMessage) // 404
+                    GithubApi.userNotFound()
+                else 
+                    GithubApi.userFound(jsonUser)
+
+            });
+
+    },
+
+    userNotFound: function() {
+
+        s.alertBox.style.display = 'block'
+
+    },
+
+    userFound: function(jsonUser) {
+
+        s.alertBox.style.display = 'none'
+        name = jsonUser["name"]
+        bio = jsonUser["bio"]
+        this.searchForRepos(function(jsonRepos) {
+
+            GithubApi.buildUserRepos(name, bio, jsonRepos)
+
+        })
+
+    },
+
+    buildUserRepos: function(name, bio, reposList) {
+
+        userInfoHtml = `<div class="user-info"><div class="github-icon"><img class="bordered" src="images/github-favicon.png"></div><div class="name-bio"><div id="username">@`+s.inputUsername.value+`</div><div id="full-name">`+name+`</div><div id="user-bio">`+bio+`</div></div></div>`
+        userReposHtml = `<div class="user-repos"><div id="repos-tittle">Repositories</div><ul>`
+        for (var i in reposList) {
+            repo = reposList[i]
+            userReposHtml += `<li><span class="repo-info"><div class="repo-info-icon"><img src="images/star.svg"></div><div>`+repo["stargazers_count"]+`</div><div class="repo-info-icon"><img src="images/repo-forked.svg"></div><div>`+repo["forks_count"]+`</div></span><span class="repo-name">`+repo["name"]+`</span></li>`
+        }
+        userReposHtml += `</ul></div>`
+
+        s.userInfoRepos.innerHTML = userInfoHtml + userReposHtml
+
+        s.userInfoRepos.style.display = 'block'
+
+
     }
 
 
@@ -63,29 +123,3 @@ GithubApi = {
 
 
 GithubApi.init()
-
-// reusable client class
-// var httpClient = function() {
-//     this.get = function(url, calllback) {
-//         var req = new XMLHttpRequest();
-//         req.onreadystatechange = function() { 
-//             if (req.readyState == 4 && req.status == 200)
-//                 calllback(req.responseText);
-//         }
-
-//         req.open( "GET", url, true );            
-//         req.send( null );
-//     }
-// }
-
-// var client = new httpClient();
-
-// fetch the user information
-// client.get("https://api.github.com/users/caspyin", function(response) {
-
-// 	// jsonize the response
-//     json = JSON.parse(json);
-
-
-// });
-
